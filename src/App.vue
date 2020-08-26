fd344a5f4138362e5793a0bff562d6a53e13d58b
superman566 <guochaohetony@gmail.com>
2020-08-26 02:39:51 -0400

    add header

--- src/App.vue
+++ src/App.vue
@@ -10,31 +10,13 @@ export default {
   },
   data(){
     return {
-      res:{}
     };
   },
   mounted() {
-    this.axios.get('/user/login').then((res)=>{
-      this.res = res;
-    });
   }
 };
 </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import './assets/scss/reset.scss';
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
